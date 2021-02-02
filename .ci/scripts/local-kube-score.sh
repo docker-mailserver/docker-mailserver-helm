fb06380f24299600f70512b18732922d1705f50d
David Young <davidy@funkypenguin.co.nz>
2021-02-02 15:06:52 +1300

    Update to 8.0.1 (#36)

---
+++ .ci/scripts/local-kube-score.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+for chart in `ls charts`;
+do
+helm template --values charts/$chart/ci/ci-values.yaml charts/$chart | kube-score score - \
+    --ignore-test pod-networkpolicy \
+    --ignore-test deployment-has-poddisruptionbudget \
+    --ignore-test deployment-has-host-podantiaffinity \
+    --ignore-test pod-probes \
+    --ignore-test container-image-tag \
+    --enable-optional-test container-security-context-privileged \
+    --ignore-test container-security-context
+done
